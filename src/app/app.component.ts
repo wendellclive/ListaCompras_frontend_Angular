d1d97b9ef2fde344fcc40246d32717fee2aa1d90
Wendell Clive Santos de Lira <wendell.clive@gmail.com>
2019-02-08 14:47:40 -0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './api.service';
+import { ShoppingItem } from './shopping-item.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'frontend';
+  items: ShoppingItem[];
+  error: any;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+    this.api.getShoppingItems().subscribe(
+      (items: ShoppingItem[]) => this.items = items,
+      (error: any) => this.error = error
+    );
+  }
+
+  add(itemName: string, itemQuantity: number) {
+    this.api.createShoppingItem(itemName, itemQuantity).subscribe(
+      (item: ShoppingItem) => this.items.push(item)
+    );
+  }
+
+  delete(id: number) {
+    this.api.deleteShoppingItem(id).subscribe(
+      (success: any) => this.items.splice(
+        this.items.findIndex(item => item.id === id)
+      )
+    );
+  }
 }
